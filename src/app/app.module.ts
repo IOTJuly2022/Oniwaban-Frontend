e809d64debc01b79ccb0dde717c24c29261811bd
Narayan <959099@cognizant.com>
2022-08-16 14:26:22 -0400

    added components & services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
+import { AboutComponent } from './about/about.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductListComponent,
+    ProductComponent,
+    AboutComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
