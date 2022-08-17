2dd004e58b98de8f14654e35265b374bb7d1b902
Bacchiani <943525@cognizant.com>
2022-08-17 10:15:16 -0500

    added routing for cart and about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
   {path:"", component:HomeComponent},
   {path:"products", component:ProductListComponent},
+  {path:"cart", component: CartComponent},
+  {path:"about", component:AboutComponent}
 
 ];
 
