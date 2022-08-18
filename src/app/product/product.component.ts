3f1799fcb0870136bc7b126053330833b976811c
Nandith-Narayan <nandith2ndemail@gmail.com>
2022-08-17 21:53:11 -0400

    added notification upon adding/removing from cart

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { CartService } from '../cart.service';
 import { Router } from '@angular/router';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-product',
@@ -13,24 +14,42 @@ export class ProductComponent implements OnInit {
   @Input() added: boolean = true;
 
 
-  constructor(private cartService: CartService, private router:Router) { }
+  constructor(private cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   addToCart(): void {
     this.cartService.addToCart(this.product);
-    alert("added to cart");
+    this.sendAlert("Added "+this.product.name+" to Cart");
     console.log(this.cartService.listCartProducts());
   }
 
   removeFromCart(): void {
     this.cartService.removeFromCart(this.product);
-    alert("removed from cart");
+    this.sendAlert("Removed "+this.product.name+" from Cart");
     console.log(this.cartService.listCartProducts());
   }
   viewProduct(): void {
-    this.router.navigate(['/viewproduct', this.product.id-1]);
+    this.router.navigate(['/viewproduct', this.product.id - 1]);
+  }
+
+  sendAlert(msg: string): void {
+    let notificationDiv = document.getElementById("notification");
+    if (notificationDiv != null) {
+      notificationDiv.innerHTML = "<h3>"+msg+"</h3>";
+      notificationDiv.style.height = "3em";
+      setTimeout(()=>{this.retractAlert();}, 5000);
+    }
+  }
+  
+
+  retractAlert():void{
+    let notificationDiv = document.getElementById("notification");
+    if (notificationDiv != null) {
+      notificationDiv.style.height = "0";
+      //notificationDiv.innerText = "";
+    }
   }
 
 }
