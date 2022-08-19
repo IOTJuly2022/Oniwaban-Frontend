a975abfc64af9ec513cf7d4153906a361aab2387
Nandith-Narayan <nandith2ndemail@gmail.com>
2022-08-19 15:42:55 -0400

    idk

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../product';
@@ -12,12 +13,24 @@ import { ReviewService } from '../review.service';
 })
 export class ReviewComponent implements OnInit {
   id = parseInt(this.routes.snapshot.paramMap.get("id")!);
-  product!:Product;
-  constructor(private routes: ActivatedRoute, private productService:ProductService, private changeDetection: ChangeDetectorRef) { }
+  product!: Product;
+  rating: string = "5";
+  description: string = "";
+
+  constructor(private routes: ActivatedRoute, private productService: ProductService, private changeDetection: ChangeDetectorRef, private http: HttpClient) { }
 
   ngOnInit(): void {
     console.log(this.id);
-    this.productService.getProductById(this.id).then(p=>{this.product = p;this.changeDetection.detectChanges();});
+    this.productService.getProductById(this.id).then(p => { this.product = p; this.changeDetection.detectChanges(); });
   }
 
+  submitReview() {
+    console.log(this.rating);
+    console.log(this.description);
+    let r_id = this.randomInteger(5000,6000);
+    let p_id = this.product.id;
+  }
+  randomInteger(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
